33f2c4ebaee7101f5df0213033e4d1de5435ac69
Mineev <mineev.buryat@gmail.com>
2023-03-11 15:32:00 +0800

    session and num_visits

--- library/libcatalog/views.py
+++ library/libcatalog/views.py
@@ -14,7 +14,9 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
-
+     # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits+1
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
@@ -23,7 +25,8 @@ def index(request):
         context={'num_books':num_books,
                  'num_instances':num_instances,
                  'num_instances_available':num_instances_available,
-                 'num_authors':num_authors},
+                 'num_authors':num_authors,
+                 'num_visits': num_visits},
     )
     
 class BookListView(generic.ListView):
