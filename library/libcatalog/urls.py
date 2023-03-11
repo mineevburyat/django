c101fc272bac98727c8a2d76a1a81eac2a85d18a
Mineev <mineev.buryat@gmail.com>
2023-03-11 13:04:35 +0800

    authors list and details

--- library/libcatalog/urls.py
+++ library/libcatalog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import index, BookListView, BookDetailView
+from .views import index, BookListView, \
+                    BookDetailView, \
+                    AuthorListView, \
+                    AuthorDetailView
 # from django.conf.urls import url
 from django.urls import re_path
 
@@ -8,5 +11,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('books/', BookListView.as_view(), name='books' ),
     path('book/<slug>', BookDetailView.as_view(), name='book-detail'),
+    path('authors/', AuthorListView.as_view(), name='authors' ),
+    path('author/<slug>', AuthorDetailView.as_view(), name='author-detail'),
     # re_path(r'^book/(?P<slug>+)$', BookDetailView.as_view(), name='book-detail'),
 ]
